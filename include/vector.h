4e9709bc4a3b5ea7e5d05508f5f5c97bc45c7d26
GiulioLV98 <giulioleoni5@gmail.com>
2024-01-19 16:25:24 +0100

    Add and get

--- include/vector.h
+++ include/vector.h
@@ -5,7 +5,8 @@
 
 typedef struct vector_t
 {
-
+    void** items;
+    size_t count;
 } vector_t;
 
 /// @brief Create a new empty vector
@@ -20,5 +21,18 @@ void vector_destroy(vector_t* vector);
 /// @param vector the vector to be checked
 /// @return true if empty, false otherwise
 bool vector_is_empty(vector_t* vector);
+
+/// @brief add one item to the vector
+/// @param vector pointer to the vector
+/// @param item pointer to the item to be added
+void vector_add(vector_t* vector, void* item);
+
+/// @brief get the item in the vector at the specified index
+/// @param vector pointer to the vector
+/// @param index index of the desired item
+/// @return item of the vector
+void* vector_get(vector_t* vector, size_t index);
+
+
 
 #endif //VECTOR_H
--- test/vector_test.c
+++ test/vector_test.c
@@ -12,3 +12,24 @@ CLOVE_TEST(CreateNewEmptyVector)
 
     vector_destroy(vector);
 }
+
+CLOVE_TEST(AddOneElement)
+{
+    vector_t* vector = vector_new();
+    int element = 1;
+    vector_add(vector, &element);
+    CLOVE_PTR_EQ(&element, vector->items[0]);
+    CLOVE_SIZET_EQ(1, vector->count);
+    vector_destroy(vector);
+}
+
+CLOVE_TEST(GetElement)
+{
+    vector_t* vector = vector_new();
+    int element = 1;
+    vector_add(vector, &element);
+    size_t index = 0;
+    void* item = vector_get(vector, index);
+    CLOVE_PTR_EQ(&element, item);
+}
+
